6209a584f624cba026ab50962c3fd79bc3105cf6
NicolasNNG <niko730sha@gmail.com>
2023-12-06 23:05:39 -0300

    cambios en back

--- BackEnd-PF-developer/src/controllers/paymentControllers.js
+++ BackEnd-PF-developer/src/controllers/paymentControllers.js
@@ -1,10 +1,8 @@
 const axios = require("axios");
 const sendEmail = require("../config/mailer.js");
-const {
-  PAYPAL_API,
-  PAYPAL_API_CLIENT,
-  PAYPAL_API_SECRET,
-} = require("../config/config.js");
+const PAYPAL_API = "https://api-m.sandbox.paypal.com";
+const PAYPAL_API_CLIENT = "AaHhTpPS12nRr6xYKtC1ON5uepgecESSzzVPZ-GF91aq-hIqPC-_Qs6csNxmCxl4-pI5SmPMZeB-aHV6"
+const PAYPAL_API_SECRET = "EAbiJOvD1WyHZ3kGeDdP3gKReovmZ_urdMRuMtDS2jF3dw1UYPmrTXkQIIyfGNzjd9dTzdvCsS0Agh8f"
 
 const createOrder = async (req, res) => {
   try {
@@ -28,8 +26,8 @@ const createOrder = async (req, res) => {
       landing_page: "LOGIN",
       user_action: "PAY_NOW",
       shipping_preference: "NO_SHIPPING",
-      return_url: "https://autenticacion-prueba-ryqg-754xpv16t-nicolas-s-projects-9dbaafa4.vercel.app/payment/capture-order",
-      cancel_url: "https://autenticacion-prueba-ryqg-754xpv16t-nicolas-s-projects-9dbaafa4.vercel.app/payment/cancel-order",
+      return_url: "http://localhost:3001/payment/capture-order",
+      cancel_url: "http://localhost:3001/payment/cancel-order",
     },
   };
 
--- FrontEnd-PF-developer/src/redux/actions.js
+++ FrontEnd-PF-developer/src/redux/actions.js
@@ -248,65 +248,65 @@ export const deleteFavorite = (datos) => async () => {
 
 
 
-// export const getApiJWT = (token) => {
-//   return new Promise((resolve, reject) => {
-//     try {
-//       let options = {
-//         method: "GET",
-//         url: "/auth",
-//         headers: {
-//           authorization: `Bearer ${token}`,
-//         },
-//       };
+export const getApiJWT = (token) => {
+  return new Promise((resolve, reject) => {
+    try {
+      let options = {
+        method: "GET",
+        url: "/auth",
+        headers: {
+          authorization: `Bearer ${token}`,
+        },
+      };
 
-//       axios.request(options).then((res) => {
-//         resolve(res.data);
-//       });
-//     } catch (err) {
-//       console.log(">> ERROR");
-//       reject(err);
-//     }
-//   });
-// };
+      axios.request(options).then((res) => {
+        resolve(res.data);
+      });
+    } catch (err) {
+      console.log(">> ERROR");
+      reject(err);
+    }
+  });
+};
 
-// export const getUsers = (apiToken) => {
-//   return new Promise((resolve, reject) => {
-//     let options = {
-//       method: "GET",
-//       url: `/users`,
-//       headers: {
-//         authorization: `Bearer ${apiToken}`,
-//       },
-//     };
+export const getUsers = (apiToken) => {
+  return new Promise((resolve, reject) => {
+    let options = {
+      method: "GET",
+      url: `/users`,
+      headers: {
+        authorization: `Bearer ${apiToken}`,
+      },
+    };
 
-//     axios
-//       .request(options)
-//       .then((response) => {
-//         resolve(response);
-//       })
-//       .catch((err) => {
-//         reject(err);
-//       });
-//   });
-// };
+    axios
+      .request(options)
+      .then((response) => {
+        resolve(response);
+      })
+      .catch((err) => {
+        reject(err);
+      });
+  });
+};
 
-// export const getUser = (userId, apiToken) => {
-//   return new Promise((resolve, reject) => {
-//     let options = {
-//       method: "GET",
-//       url: `/users/${userId}`,
-//       headers: {
-//         authorization: `Bearer ${apiToken}`,
-//       },
-//     };
+export const getUser = (userId, apiToken) => {
+  return new Promise((resolve, reject) => {
+    let options = {
+      method: "GET",
+      url: `/users/${userId}`,
+      headers: {
+        authorization: `Bearer ${apiToken}`,
+      },
+    };
 
-//     axios
-//       .request(options)
-//       .then((response) => {
-//         resolve(response);
-//       })
-//       .catch((err) => {
-//         reject(err);
-//       });
-//   });
-// };
+    axios
+      .request(options)
+      .then((response) => {
+        resolve(response);
+      })
+      .catch((err) => {
+        reject(err);
+      });
+  });
+};
