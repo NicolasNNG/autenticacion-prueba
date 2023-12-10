41bfcdfe6516843ae7203f675c3c314340ff3e8b
NicolasNNG <niko730sha@gmail.com>
2023-12-10 14:30:51 -0300

    cambios -developer

--- FrontEnd-PF-developer/next.config.js
+++ FrontEnd-PF-developer/next.config.js
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        domains: ['static.nike.com', "assets.adidas.com"],
-      },
+  images: {
+      domains: ['static.nike.com', "assets.adidas.com", "lh3.googleusercontent.com"],
+    },
 }
 
 module.exports = nextConfig
